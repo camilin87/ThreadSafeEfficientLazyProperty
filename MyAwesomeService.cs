2d7a192269a2a4e47357013c699c81640efb53b3
Camilo Sanchez <camilin87@gmail.com>
2018-02-28 07:53:25 -0500

    first implementation of a ThreadSafe and efficient lazy service

---
+++ MyAwesomeService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ThreadSafeEfficientLazyProperty
+{
+  public class MyAwesomeService
+  {
+    private object criticalSection = new object();
+    private Func<ExpensiveObject> expensiveLoad;
+
+    public MyAwesomeService()
+    {
+      expensiveLoad = createAndCacheExpensiveLoad;
+    }
+
+    public ExpensiveObject ExpensiveLoad
+    {
+      get { return expensiveLoad(); }
+    }
+
+    private class ExpensiveObjectFactory
+    {
+      private readonly ExpensiveObject result = new ExpensiveObject();
+
+      public ExpensiveObject Build() => result;
+    }
+    
+    private ExpensiveObject createAndCacheExpensiveLoad()
+    {
+      lock (criticalSection)
+      {
+        if (expensiveLoad == createAndCacheExpensiveLoad)
+        {
+          expensiveLoad = new ExpensiveObjectFactory().Build;
+        }
+      }
+      
+      return expensiveLoad();
+    }
+    
+    public void DoWork()
+    {
+      Console.WriteLine("MyAwesomeService: Starting work");
+      
+      ExpensiveLoad.Move(10, 2000);
+      ExpensiveLoad.Move(2000, 15);
+      
+      Console.WriteLine("MyAwesomeService: work completed");
+    }
+  }
+}

1fe9839b87f3332a8ff88cabc3a54dc46daea67e
Camilo Sanchez <camilin87@gmail.com>
2018-02-28 08:21:49 -0500

    it works

--- MySimplifiedAwesomeService.cs
+++ MySimplifiedAwesomeService.cs
@@ -24,13 +24,40 @@ namespace ThreadSafeEfficientLazyProperty
 
     private class LazyPropertyHelperInternal<T>
     {
+      private readonly object _criticalSection = new object();
+      private Func<T> _expensiveLoadReader;
+
       private readonly Func<T> _initializer;
     
-      public LazyPropertyHelperInternal(Func<T> initializer) => _initializer = initializer;
+      public T Read() => ExpensiveLoad;
+      private T ExpensiveLoad => _expensiveLoadReader();
 
-      public T Read()
+      public LazyPropertyHelperInternal(Func<T> initializer)
       {
-        return _initializer();
+        _initializer = initializer;
+        _expensiveLoadReader = createAndCacheExpensiveLoad;
+      }     
+
+      private class ExpensiveLoadFactory<T>
+      {
+        private readonly T result;
+
+        public ExpensiveLoadFactory(Func<T> initializer) => result = initializer();
+
+        public T Build() => result;
+      }
+      
+      private T createAndCacheExpensiveLoad()
+      {
+        lock (_criticalSection)
+        {
+          if (_expensiveLoadReader == createAndCacheExpensiveLoad)
+          {
+            _expensiveLoadReader = new ExpensiveLoadFactory<T>(_initializer).Build;
+          }
+        }
+      
+        return _expensiveLoadReader();
       }
     }
   }

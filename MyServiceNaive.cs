8128771c10afda42390fc55f02c727f27d11561b
Camilo Sanchez <camilin87@gmail.com>
2018-02-28 07:09:51 -0500

    create MyServiceNaive that uses the Expensive object

---
+++ MyServiceNaive.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ThreadSafeEfficientLazyProperty
+{
+  public class MyServiceNaive
+  {
+    private ExpensiveObject expensiveLoad;
+
+    public ExpensiveObject ExpensiveLoad
+    {
+      get
+      {
+        if (expensiveLoad == null)
+        {
+          expensiveLoad = new ExpensiveObject();
+        }
+
+        return expensiveLoad;
+      }
+    }
+
+    public void DoWork()
+    {
+      Console.WriteLine("MyServiceNaive: Starting work");
+      
+      ExpensiveLoad.Move(10, 2000);
+      ExpensiveLoad.Move(2000, 15);
+      
+      Console.WriteLine("MyServiceNaive: work completed");
+    }
+  }
+}

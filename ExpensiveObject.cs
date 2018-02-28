56aae6c91d8215483e5b992e4053ec9fe56a1a41
Camilo Sanchez <camilin87@gmail.com>
2018-02-27 20:31:41 -0500

    create an empty project with an expensive object

---
+++ ExpensiveObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadSafeEfficientLazyProperty
+{
+  public class ExpensiveObject
+  {
+    public ExpensiveObject()
+    {
+      Console.WriteLine("Starting Expensive object creation");
+
+      Thread.Sleep(2000);
+      
+      Console.WriteLine("Completed Expensive object creation");
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThreadSafeEfficientLazyProperty
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Lazy Expensive Thread-Safe efficient property");
+    }
+  }
+}
